ccc0e59367b78364e2ea08655b001cffc2681b78
ofer adel <12687466+CptSchnitz@users.noreply.github.com>
2020-07-20 11:14:27 +0300

    conflict store UT

--- mc-conflicts-ui/src/conflicts/models/conflictStore.spec.ts
+++ mc-conflicts-ui/src/conflicts/models/conflictStore.spec.ts
@@ -1,78 +1,101 @@
 import * as fs from 'fs';
 import { rootStore } from './rootStore';
 import { when } from 'mobx';
+import { IConflictsStore } from './conflictStore';
 
-const locations = [{
-  properties: {},
-  type: 'Feature',
-  geometry: {
-    type: "Point",
-    coordinates: [35.2137, 31.7683]
-  }
-},
-{
-  properties: {},
-  type: 'Feature',
-  geometry: {
-    type: "Point",
-    coordinates: [34.7818, 32.0853]
-  }
-},
-{
-  properties: {},
-  type: 'Feature',
-  geometry: {
-    type: "Point",
-    coordinates: [34.9896, 32.794]
-  }
-}];
+const locations = [
+  {
+    properties: {},
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [35.2137, 31.7683],
+    },
+  },
+  {
+    properties: {},
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [34.7818, 32.0853],
+    },
+  },
+  {
+    properties: {},
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [34.9896, 32.794],
+    },
+  },
+];
 
-const conflicts = JSON.parse(fs.readFileSync("./public/conflicts.json").toString());
-const conflictFetcher = () => Promise.resolve(conflicts);
+const conflicts = JSON.parse(
+  fs.readFileSync('./public/conflicts.json').toString()
+);
+
+const conflictSpy = jest.fn().mockReturnValue(conflicts);
+
+const conflictFetcher = () => new Promise((resolve) => resolve(conflictSpy()));
+
+let conflictsStore: IConflictsStore;
+
+beforeEach(() => {
+  conflictsStore = rootStore.create({}, { fetch: conflictFetcher }).conflictsStore;
+});
+
+afterEach(() => {
+  conflictSpy.mockClear();
+})
 
 it('return an array of features of the conflicts location', async () => {
-  const { conflictsStore } = rootStore.create({}, { fetch: conflictFetcher });
-
   await when(() => conflictsStore.state === 'done');
   const result = conflictsStore.conflictLocations;
 
-  expect(result).toEqual(locations)
-})
+  expect(result).toEqual(locations);
+});
 
 it('sets the selected conflict to the correct value', () => {
-  const { conflictsStore } = rootStore.create({}, { fetch: conflictFetcher });
   conflictsStore.selectConflict(conflicts[0]);
 
   conflictsStore.resetSelectedConflict();
 
   expect(conflictsStore.selectedConflict).toBeUndefined();
-})
+});
 
 it('resetSelection set the selected conflict to undefined', () => {
-  const { conflictsStore } = rootStore.create({}, { fetch: conflictFetcher });
   conflictsStore.selectConflict(conflicts[0]);
 
   conflictsStore.resetSelectedConflict();
 
   expect(conflictsStore.selectedConflict).toBeUndefined();
-})
+});
 
 it('format and set the conflicts, and set state to done on fetch', async () => {
   expect.assertions(2);
-  const { conflictsStore } = rootStore.create({}, { fetch: conflictFetcher });
 
   await conflictsStore.fetchConflicts();
 
-  expect(conflictsStore.state).toEqual('done')
+  expect(conflictsStore.state).toEqual('done');
   expect(conflictsStore.conflicts.length).toEqual(3);
-})
+});
 
 it('set state to error when there is an error fetching the conflicts', async () => {
   expect.assertions(1);
-  const { conflictsStore } = rootStore.create({}, { fetch: () => Promise.reject(new Error()) });
+  const errorConflictStore = rootStore.create(
+    {},
+    { fetch: () => Promise.reject(new Error()) }
+  ).conflictsStore;
 
-  await conflictsStore.fetchConflicts();
+  await errorConflictStore.fetchConflicts();
 
-  expect(conflictsStore.state).toEqual('error')
+  expect(errorConflictStore.state).toEqual('error');
+});
 
+it('calls fetch conflicts on search params snapshot', () => {
+  conflictSpy.mockClear();
+  
+  conflictsStore.searchParams.setResolved(true);
+
+  expect(conflictSpy).toHaveBeenCalledTimes(1);
 })
