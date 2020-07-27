c5f5071faf21755e3f3862fb78d4f35aceb0a4ea
--list <12687466+CptSchnitz@users.noreply.github.com>
2020-07-27 11:47:35 +0300

    removed onSelected from ConflictItemProps

--- mc-conflicts-ui/src/conflicts/components/conflict-item.tsx
+++ mc-conflicts-ui/src/conflicts/components/conflict-item.tsx
@@ -4,19 +4,18 @@ import Typography from '@material-ui/core/Typography';
 import { IConflict } from '../models/conflict';
 
 interface ConflictItemProps {
-  conflict: IConflict,
-  onSelected?: (conflict: IConflict) => void
+  conflict: IConflict;
 }
 
 const ConflictItem: React.FC<ConflictItemProps> = ({ conflict }) => {
-
   return (
     <div>
       <Typography variant="subtitle1">{'#' + conflict.id}</Typography>
       <div>{conflict.source_server}</div>
       <div>{conflict.target_server}</div>
       <div>{conflict.description}</div>
-    </div>)
-}
+    </div>
+  );
+};
 
 export default ConflictItem;
