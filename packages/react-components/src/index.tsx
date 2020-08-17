6113a9b3589a43effe38fb5795cc90bed564f6e6
LEBINSON ALEX <ALEXLEB@rf.local>
2020-08-17 13:12:42 +0300

    fix(cleanup): unneeded storybooks remains

--- packages/react-components/src/index.tsx
+++ packages/react-components/src/index.tsx
@@ -1,27 +1,5 @@
-import React,{ useMemo } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import { useDarkMode } from 'storybook-dark-mode';
-
-
-export const ThemeWrapper:React.FC = (props) => {
-  const prefersDarkMode = useDarkMode();
-
-  const muiTheme = useMemo(
-    () =>
-      createMuiTheme({
-        palette: {
-          type: prefersDarkMode ? 'dark' : 'light',
-        },
-      }),
-    [prefersDarkMode]
-  );
-
-  return <ThemeProvider theme={muiTheme}>
-      {props.children}
-    </ThemeProvider>
-};
-
 
 ReactDOM.render(
   <React.StrictMode>
