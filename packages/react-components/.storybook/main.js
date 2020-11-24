1165b8d8d887560da79552d4feb31452fe0f126c
alebinson <newdarkvirus@gmail.com>
2020-11-24 19:00:38 +0200

    fix(prettier): fix

--- packages/react-components/.storybook/main.js
+++ packages/react-components/.storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   stories: [
     '../src/lib/date-range-picker/stories/DateRangePicker.stories.tsx',
-    '../src/**/*.stories.tsx'
+    '../src/**/*.stories.tsx',
   ],
   addons: [
     '@storybook/preset-create-react-app',
