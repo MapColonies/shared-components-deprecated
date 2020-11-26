56147540403d00cf2b59fdfc72f81bbe91d30708
ALEXANDER LEVINSON <ALEXLEB@rf.local>
2020-11-26 12:05:42 +0200

    feat(cesium-entity): component added

---
+++ packages/react-components/src/lib/cesium-map/entities/entity.description.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { 
+  EntityDescription as ResiumEntityDescription,
+  EntityStaticDescription as ResiumEntityStaticDescription,
+
+} from 'resium';
+import { EntityDescriptionProps } from 'resium/dist/types/src/EntityDescription/EntityDescription';
+
+export interface RCesiumEntityDescriptionProps extends EntityDescriptionProps {}
+
+export const CesiumEntityDescription: React.FC<RCesiumEntityDescriptionProps> = (
+  props
+) => {
+  return <ResiumEntityDescription {...props} />;
+};
+
+export const CesiumEntityStaticDescription: React.FC = (
+  props
+) => {
+  return <ResiumEntityStaticDescription {...props} />;
+};
---
+++ packages/react-components/src/lib/cesium-map/entities/entity.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Entity as ResiumEntity } from 'resium';
+import { EntityProps } from 'resium/dist/types/src/Entity/Entity';
+
+export interface RCesiumEntityProps extends EntityProps {}
+
+export const CesiumEntity: React.FC<RCesiumEntityProps> = (
+  props
+) => {
+  return <ResiumEntity {...props} />;
+};
