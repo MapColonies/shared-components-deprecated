1ef0cb65d7344a6215fac12ede668bcac2bee718
ALEXANDER LEVINSON <ALEXLEB@rf.local>
2020-12-08 14:20:07 +0200

    fix(typing): fixed

--- packages/react-components/src/lib/cesium-map/tools/geojson/geojson-to-primitive.ts
+++ packages/react-components/src/lib/cesium-map/tools/geojson/geojson-to-primitive.ts
@@ -1,4 +1,4 @@
-import { FeatureCollection, Feature } from 'geojson';
+import { FeatureCollection, Feature, Point } from 'geojson';
 import { Rectangle } from 'cesium';
 import { PrimitiveCoordinates } from '../../data-sources';
 import { DrawType } from '../../../models/enums';
@@ -12,12 +12,11 @@ export const geoJSONToPrimitive = (
       if(geojson.features.length !== 2)
         throw new Error(`${type} must have 2 points`);
 
-      /* eslint-disable @typescript-eslint/no-unsafe-member-access */
       return Rectangle.fromDegrees(
-        (geojson.features[1].geometry as Feature).coordinates[0],
-        (geojson.features[1].geometry as Feature).coordinates[1],
-        (geojson.features[0].geometry as Feature).coordinates[0],
-        (geojson.features[0].geometry as Feature).coordinates[1],
+        (geojson.features[1] as Feature<Point>).geometry.coordinates[0],
+        (geojson.features[1] as Feature<Point>).geometry.coordinates[1],
+        (geojson.features[0] as Feature<Point>).geometry.coordinates[0],
+        (geojson.features[0] as Feature<Point>).geometry.coordinates[1],
       );
     default:
       throw new Error(`${type} type geoJSON still not supported`);
