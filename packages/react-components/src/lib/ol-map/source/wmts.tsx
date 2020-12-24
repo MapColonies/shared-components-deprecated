e6697b0e95405e07c2e6fc34107f8d21bb0cab18
Alex Lebinson <newdarkvirus@gmail.com>
2020-12-24 18:32:34 +0200

    fix(wmts-layer-config): proper configs are passed + story (#86)
    * fix(wmts-layer-config): proper configs are passed + story
    
    * chore(prettier): fixes

--- packages/react-components/src/lib/ol-map/source/wmts.tsx
+++ packages/react-components/src/lib/ol-map/source/wmts.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from 'react';
 import { WMTS } from 'ol/source';
 import { Options } from 'ol/source/WMTS';
+import WMTSRequestEncoding from 'ol/source/WMTSRequestEncoding';
 import { get as getProjection } from 'ol/proj';
 import WMTSTileGrid from 'ol/tilegrid/WMTS';
 import { getTopLeft, getWidth } from 'ol/extent';
 import { useTileLayer } from '../layers/tile-layer';
 
-const RESOLUTIONS = 14,
+const RESOLUTIONS = 20,
   TILE_GRANULARITY = 256,
-  WMTS_RESOLUTION_BASIS = 2;
+  WMTS_RESOLUTION_BASIS = 2,
+  LAYER_HEIGHT_WIDHT_RATIO = 2,
+  REQUEST_ENCODING = 'REST';
 
 interface TileWMTSProps {
   options: Options;
@@ -20,7 +23,11 @@ export interface WMTSOptionParams {
   layer: string;
   projection: string;
   format: string;
+  style: string;
+  matrixSet: string;
   wrapX?: boolean;
+  heightWidthRatio?: number;
+  requestEncoding?: WMTSRequestEncoding | string;
 }
 
 export const getWMTSOptions = (params: WMTSOptionParams): Options => {
@@ -29,9 +36,18 @@ export const getWMTSOptions = (params: WMTSOptionParams): Options => {
   const resolutions = new Array<number>(RESOLUTIONS);
   const matrixIds = new Array<string>(RESOLUTIONS);
   const size = getWidth(projectionExtent) / TILE_GRANULARITY;
+  const heightWidthRatio =
+    params.heightWidthRatio !== undefined
+      ? params.heightWidthRatio
+      : LAYER_HEIGHT_WIDHT_RATIO;
+  const requestEncoding =
+    params.requestEncoding !== undefined
+      ? params.requestEncoding
+      : REQUEST_ENCODING;
   for (let z = 0; z < RESOLUTIONS; ++z) {
     // generate resolutions and matrixIds arrays for this WMTS
-    resolutions[z] = size / Math.pow(WMTS_RESOLUTION_BASIS, z);
+    resolutions[z] =
+      size / Math.pow(WMTS_RESOLUTION_BASIS, z) / heightWidthRatio;
     matrixIds[z] = z.toString();
   }
 
@@ -39,7 +55,7 @@ export const getWMTSOptions = (params: WMTSOptionParams): Options => {
     attributions: params.attributions,
     url: params.url,
     layer: params.layer,
-    matrixSet: params.projection,
+    matrixSet: params.matrixSet,
     format: params.format,
     projection: projection,
     tileGrid: new WMTSTileGrid({
@@ -47,7 +63,8 @@ export const getWMTSOptions = (params: WMTSOptionParams): Options => {
       resolutions: resolutions,
       matrixIds: matrixIds,
     }),
-    style: 'default',
+    style: params.style,
+    requestEncoding: requestEncoding,
     wrapX: params.wrapX !== undefined ? params.wrapX : true,
   };
 
