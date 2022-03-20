ee59db00f627b709368f830a3c7cf30dc2cf0525
Ella <ellamartirosyan@gmail.com>
2022-03-20 13:12:00 +0200

    fix: 3d story

--- packages/react-components/src/lib/cesium-map/layers/3d.tileset.stories.tsx
+++ packages/react-components/src/lib/cesium-map/layers/3d.tileset.stories.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { ArcGISTiledElevationTerrainProvider } from 'cesium';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import { action } from '@storybook/addon-actions';
 import { CesiumMap } from '../map';
+import { LayerType } from '../layers-manager';
 import { Cesium3DTileset } from './3d.tileset';
 
 export default {
@@ -18,12 +20,47 @@ const mapDivStyle = {
   position: 'absolute' as const,
 };
 
+const BASE_MAPS = {
+  maps: [
+    {
+      id: '1st',
+      title: '1st Map Title',
+      isCurrent: true,
+      thumbnail:
+        'https://nsw.digitaltwin.terria.io/build/efa2f6c408eb790753a9b5fb2f3dc678.png',
+      baseRasteLayers: [
+        {
+          id: 'GOOGLE_TERRAIN',
+          type: 'XYZ_LAYER' as LayerType,
+          opacity: 1,
+          zIndex: 0,
+          options: {
+            url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+            layers: '',
+            credit: 'GOOGLE',
+          },
+        },
+      ],
+      baseVectorLayers: [],
+    },
+  ],
+};
+
+const ArcGisProvider = new ArcGISTiledElevationTerrainProvider({
+  url:
+    'https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer',
+});
+
 export const Cesium3DTilesetLayer: Story = (args: unknown) => (
   <div style={mapDivStyle}>
-    <CesiumMap {...args}>
+    <CesiumMap
+      baseMaps={BASE_MAPS}
+      terrainProvider={ArcGisProvider}
+      {...args}
+    >
       <Cesium3DTileset
         isZoomTo={true}
-        url="/mock/tileset/tileset.json"
+        url="/mock/tileset_1/tileset.json"
         onAllTilesLoad={action('onAllTilesLoad')}
         onInitialTilesLoad={action('onInitialTilesLoad')}
         onTileFailed={action('onTileFailed')}
@@ -51,11 +88,15 @@ Cesium3DTilesetLayer.argTypes = {
 
 export const Cesium3DTilesetOnHeightLayer: Story = (args: unknown) => (
   <div style={mapDivStyle}>
-    <CesiumMap {...args}>
+    <CesiumMap
+      baseMaps={BASE_MAPS}
+      terrainProvider={ArcGisProvider}
+      {...args}
+    >
       <Cesium3DTileset
         isZoomTo={false}
         heightFromGround={-170}
-        url="/mock/tileset/tileset.json"
+        url="/mock/tileset_1/tileset.json"
         onAllTilesLoad={action('onAllTilesLoad')}
         onInitialTilesLoad={action('onInitialTilesLoad')}
         onTileFailed={action('onTileFailed')}
@@ -86,7 +127,11 @@ Cesium3DTilesetOnHeightLayer.argTypes = {
 
 export const CesiumSolar3DTilesetLayer: Story = (args: unknown) => (
   <div style={mapDivStyle}>
-    <CesiumMap {...args}>
+    <CesiumMap
+      baseMaps={BASE_MAPS}
+      terrainProvider={ArcGisProvider}
+      {...args}
+    >
       <Cesium3DTileset
         isZoomTo={true}
         url="/mock/tileset_2/L16_31023/L16_31023.json"
