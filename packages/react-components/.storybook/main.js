932c4455c35a0d93bd98bdbe134821e763fcdcfe
Assaf Chamoy <assafchamoy97@gmail.com>
2023-06-18 20:02:34 +0300

    feat: trying to compile storybook, partially works, need fixes

--- packages/react-components/.storybook/main.js
+++ packages/react-components/.storybook/main.js
@@ -10,4 +10,22 @@ module.exports = {
     '@storybook/addon-links',
     'storybook-dark-mode',
   ],
+  webpackFinal: async (config) => {
+    const rules = config.module.rules;
+    const extensions = config.resolve.extensions;
+
+    rules.push({
+      test: /(\.mjs|\.js|\.jsx|\.ts|\.tsx)$/,
+      // exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    });
+    // extensions.push('.ts','.js','.jsx')
+
+    return config;
+  },
 };
